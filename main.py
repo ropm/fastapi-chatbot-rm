b817e85fe5492ed0a034aa34b94392f008af84bd
Roope Marttinen <roope.marttinen@hotmail.com>
2021-01-29 18:34:53 +0200

    ready for production

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
+    "http://localhost:8000",
     "http://localhost:8080",
+    "http://127.0.0.1:8000"
 ]
 
 app.include_router(routes.router, prefix='/api')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
